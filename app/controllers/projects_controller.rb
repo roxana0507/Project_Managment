728437e6763c7302a3fa18efff0b6ae544f88e3e
Roxana Chinchilla <roxy.0507@gmail.com>
2021-03-05 21:07:31 -0600

    2 commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,37 @@
+class ProjectsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
+  def index
+      @projects = Project.all
+      if params[:status] and  params[:status] != 'all'
+        @projects = @projects.where(status: params[:status])
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy 
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to dashboard_path
+    end
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :started_at, :ended_at, :status)
+  end
+end 
